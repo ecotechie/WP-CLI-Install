0b331072e4012d1652d79ec5705ff27b5771f07f
Sergio Scabuzzo <sergio@thegreenmanproject.com>
2019-03-04 19:04:09 -0800

    Add script and .gitignore

---
+++ wp-cli-install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+# If wpcli is not installed and user is not "root"
+if ! [[ wp cli version ]] && [[ "$EUID" -ne 0  ]]; then
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mkdir -p ~/bin
+	mv wp-cli.phar ~/bin/wp
+	cat "export PATH=~/bin:$PATH" >> ~/.bashrc
+	. ~/.bashrc
+	wp cli version
+fi
+
+exit 1
