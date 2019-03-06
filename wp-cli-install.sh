f08bc4027d62b091b5c2ce28696d39c5850f770a
Sergio Scabuzzo <sergio@thegreenmanproject.com>
2019-03-05 20:34:37 -0800

    Update wp-cli-install.sh
    Fix concatenation issue when trying to use cat. Switched to echo. Adding path to .bashrc

--- wp-cli-install.sh
+++ wp-cli-install.sh
@@ -3,12 +3,12 @@ set -e
 set -o pipefail
 
 # If wpcli is not installed and user is not "root"
-if ! wp cli version && "$EUID" -ne 0 ; then
+if [[ ! -x "$(command -v wp)" && "$EUID" -ne 0 ]]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mkdir -p ~/bin
 	mv wp-cli.phar ~/bin/wp
-	cat "export PATH=~/bin:$PATH" >> ~/.bashrc
+	echo "PATH=~/bin:$PATH" >> ~/.bashrc
 	. ~/.bashrc
 	wp cli version
 fi
